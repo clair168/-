de8ce50ec8f592b3820e65fd56bdfe6f0a06d978
b0983880800@gmail.com <Clair>
2023-05-10 21:30:53 +0800

    0510

---
+++ dist/js/0510 js_base.js
@@ -0,0 +1,11 @@
+let age = 18;
+let firstName = 'David';
+let lastName = 'Lin';
+let country = 'TW';
+
+// 淺藍是變數，橘色是變數的值，包起來=文字
+// '放字元' "放字串"
+
+
+console.log('(' + country + ') ' + firstName + ' ' + lastName);
+console.log(`(${country}) ${firstName} ${lastName}`);
