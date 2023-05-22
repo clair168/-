53183197e17d6debdfcea7793bf12595c8e4444c
clair <b0983880800@gmail.com>
2023-05-22 21:28:40 +0800

    0522_實作手機選單 icon 切換

--- dist/js/0522_js_dom_control.js
+++ dist/js/0522_js_dom_control.js
@@ -5,5 +5,25 @@ doing.addEventListener('click', () => {
     uid.style.color = 'red';
     uid.style.fontSize = '100px';
     uid.style.backgroundColor = '#dedede';
+});
 
+let addClass = document.querySelector('#add-class');
+addClass.addEventListener('click', () => {
+    uid.classList.add('active');
+});
+
+let removeClass = document.querySelector('#remove-class');
+removeClass.addEventListener('click', () => {
+    uid.classList.remove('active');
+});
+
+let mobileIcon = document.querySelector('#mobile-icon');
+
+
+mobileIcon.addEventListener('click', () => {
+    if (mobileIcon.classList.contains('active')) {
+        mobileIcon.classList.remove('active');
+    } else {
+        mobileIcon.classList.add('active');
+    }
 });
