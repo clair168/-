473d9874509eeb224b4a85abadb2ffd24277aa11
clair <b0983880800@gmail.com>
2023-05-31 21:32:25 +0800

    doAddItem 實作 Vue 架構

--- dist/js/0531-todolist-vue.js
+++ dist/js/0531-todolist-vue.js
@@ -1,8 +1,28 @@
 Vue.createApp({
     data() {
-        return { name: '123' }
+        return {
+            pending: [],
+            done: [],
+            itemValue: '',
+        }
     },
     methods: {
-
+        doAddItem() {
+            let Value = this.itemValue;
+            if (!value) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                    footer: '<a href="">Why do I have this issue?</a>'
+                }).then(() => {//然後要幹嘛
+                    setTimeout(() => {//單次計時器
+                        itemName.focus();
+                    }, 500)//數字太小動畫還沒跑完，0.5秒後再鎖定
+                })
+                return;
+            }
+            console.log(value);
+        }
     }
 }).mount('#app');
