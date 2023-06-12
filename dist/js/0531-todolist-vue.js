aef0ee693c62e807addc73a456c8390409862695
clair <b0983880800@gmail.com>
2023-06-12 19:51:32 +0800

    儲存到雲端空間

--- dist/js/0531-todolist-vue.js
+++ dist/js/0531-todolist-vue.js
@@ -60,8 +60,73 @@ let vm = Vue.createApp({
         update() {
             database.set('todo-pending', this.pending);
             database.set('todo-done', this.done);
-        }
+        },
+
+        doSaveCloud() {
+            Swal.fire({
+                title: 'Enter your password',
+                input: 'password',
+                inputLabel: 'Password',
+                inputPlaceholder: 'Enter your password',
+                //confirmButtonText: pageYOffset,
+                inputAttributes: {
+                    maxlength: 10,
+                }
+            }).then(response => {
+                if (response.value) {
+                    let api = 'https://book.niceinfos.com/frontend/api/';
+
+                    // 使用表單物件
+                    // let form = new FormData;
+                    // form.append('action', 'todo');
+                    // form.append('uid', response.value);
+                    // form.append('data', {
+                    //     pending: this.pending,
+                    //     done: this.done
+                    // });
+
+                    let params = {
+                        action: 'todo',
+                        uid: response.value,
+                        data: {
+                            pending: this.pending,
+                            done: this.done
+                        }
+                    }
+
+                    fetch(api, {
+                        method: 'POST',
+                        body: JSON.stringify(params)
+                    }).then(response => {
+                        return response.text();
+                    }).then(text => {
+                        setTimeout(() => {
+                            let data = JSON.parse(text);
+                            console.log(data);
+                            Swal.fire({
+                                title: '儲存完畢',
+                                text: '資料已儲存',
+                                icon: 'success'
+                            })
+                        }, 5000);
+                    })
+
+                    Swal.fire({
+                        title: '資料儲存中',
+                        text: '請勿關閉或重新整理視窗',
+                        showConfirmButton: false
+                    })
+                }
+            })
+        },
+
+
+        doLoadCloud() {
+            console.log('load');
+        },
     },
+
+
     mounted() {
         this.pending = database.get('todo-pending', []);
         console.log(this.pending);
